2c189c210c3511a5b8412efd10249fd72606f2d6
Eliot Cleveland <eclevela@richmond.edu>
2022-10-19 23:53:34 -0400

    added typdefs and resolvers and queries

--- server/server.js
+++ server/server.js
@@ -1,14 +1,38 @@
-const express = require('express');
-const db = require('./config/connection');
+const express = require("express");
+// import Apollo Server
+const { ApolloServer } = require("apollo-server-express");
+
+//import our typeDefs and resolvers
+
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
+// Create a new instance of an Apollo server with the graphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+
+  //integrate our Apollo server with the Express application middleware
+  server.applyMiddleware({ app });
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      // log where to go to test our gql api
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
   });
-});
+};
+
+startApolloServer();
