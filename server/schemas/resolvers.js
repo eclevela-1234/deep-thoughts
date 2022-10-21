4b0f480c54e967e862b271389f86973834d08cb4
Eliot Cleveland <eclevela@richmond.edu>
2022-10-20 22:23:39 -0400

    token typ def and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,8 @@
 const { User, Thought } = require("../models");
 
+const { AuthenticationError } = require('apollo-server-express');
+
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
@@ -23,6 +26,29 @@ const resolvers = {
         .populate("thoughts");
     },
   },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return {token, user};
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({email});
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect Credentials!');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect Credentials");
+      }
+      const token = signToken(user);
+      return {token, user};
+    }
+  }
 };
 
 module.exports = resolvers;
