4f823f3cc69734f42792958b7305f3fea2160a15
Eliot Cleveland <eclevela@richmond.edu>
2022-10-23 16:58:17 -0400

    updated scrips, app & server file to run in production env

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const path = require("path");
 // import Apollo Server
 const { ApolloServer } = require("apollo-server-express");
-const { authMiddleware } = require('./utils/auth');
+const { authMiddleware } = require("./utils/auth");
 //import our typeDefs and resolvers
 
 const { typeDefs, resolvers } = require("./schemas");
@@ -11,13 +12,22 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware
+  context: authMiddleware,
 });
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Server up static assets 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, '--/client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
 
 // Create a new instance of an Apollo server with the graphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
